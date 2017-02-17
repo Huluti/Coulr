6a624573f08aa9ab36dcf8f5b4733c160ae27df6
Hugo Posnic <hugo.posnic@protonmail.com>
2017-02-17 18:54:06 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="Coulr",
+      version="1.2.0",
+      description="Coulr is a color box to help developers and designers.",
+      author="Hugo Posnic",
+      url="https://github.com/Huluti/Coulr",
+      packages=["coulr"])
