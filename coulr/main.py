67c08350804500ea307c4fc459d872833011252c
Hugo Posnic <hugo.posnic@gmail.com>
2018-12-28 17:51:44 +0100

    snapcraft.yaml: Try to fix paths again and again

--- coulr/main.py
+++ coulr/main.py
@@ -20,8 +20,9 @@ class App(Gtk.Window):
         self.set_position(Gtk.WindowPosition.CENTER)
 
         # Paths
+        script_path = os.path.dirname(os.path.abspath(__file__))
         home_path = os.path.expanduser("~")
-        logo_path = "assets/{name}.png".format(name=self.app)
+        logo_path = "{}/assets/{}.png".format(script_path, self.app)
         self.save_file = "{}/.config/{}.json".format(home_path, self.app)
         self.logo = GdkPixbuf.Pixbuf.new_from_file(logo_path)
 
